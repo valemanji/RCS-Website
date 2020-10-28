0ef8a172150a44f1249ba982086c2360727d140f
valemanji <va23@rice.edu>
2020-10-28 10:41:12 -0500

    Upload website used during break

---
+++ precache-manifest.c7b073255d792c041093e5ad45436a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a494d0e6081b86c7960f269c76858d9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "91e4b741d4eb58ba751d",
+    "url": "/home/static/css/main.7d40c2c0.chunk.css"
+  },
+  {
+    "revision": "d81e7035fca0e21a4a9e",
+    "url": "/home/static/js/2.e9225322.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.e9225322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e4b741d4eb58ba751d",
+    "url": "/home/static/js/main.a3cc0abc.chunk.js"
+  },
+  {
+    "revision": "583030241acd4b517ac8",
+    "url": "/home/static/js/runtime-main.f859e3e7.js"
+  }
+]);
